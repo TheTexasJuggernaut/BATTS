f2e31a4cf9105de01023e0c2d163adcf70fc3e57
TheTexasJuggernaut <43121927+TheTexasJuggernaut@users.noreply.github.com>
2020-10-18 22:01:26 -0500

    Update Login

--- BATTS/BATTS/Views/LoginPage.xaml.cs
+++ BATTS/BATTS/Views/LoginPage.xaml.cs
@@ -14,21 +14,20 @@ using BATTS.Services;
 
 
 using System.Collections.ObjectModel;
-
-
+using System.Windows.Input;
 
 namespace BATTS.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class LoginPage : ContentPage
 	{
+        //Create a list object 10/18/20
+        public List<UserDataModel> LoginDB = new List<UserDataModel>();
+        public ICommand LoginDBVerify { get; set; }
+
         //public ObservableCollection<UserDataModel> Items { get; set; }
 
-        Entry Email;
-        Entry Password;
-
-        private string newUserEmail;
-        private string newUserPass;
+        
         UserDataModel Test = new UserDataModel();
 
         //public LoginPage ()
@@ -72,7 +71,7 @@ namespace BATTS.Views
 
         async public void DoLogin(object sender, EventArgs e)
         {
-            await Navigation.PushModalAsync(new NavigationPage(new NewItemPage()));
+            //await Navigation.PushModalAsync(new NavigationPage(new NewItemPage()));
             // if (email.Text == "****@gmail.co" && password.Text == "12345" || email.Text == newUserEmail && password.Text == newUserPass)
             //
             /// {
@@ -83,19 +82,116 @@ namespace BATTS.Views
             //  {
             //      Toast.MakeText(this, "Wrong credentials found!", ToastLength.Long).Show();
             //  }
-            var getUser = await AzuCosmoDBManager.GetUserData();
-            string xx = "xx";
-            for (int x =0; x <= 100; ++x)
-            {
-                string xWx = "xx";
-            }
-            await Navigation.PushModalAsync(new NavigationPage(new Menu()));
+            // var getUser = await AzuCosmoDBManager.GetUserData(); 10/18/20
 
+            //Copies from Cloud DB to Local List DB 10/18/20
+           // LoginDBVerify = new Command(async () => await ExecuteLoginDBVerify());
+            //Copies from Cloud DB to Local List DB 10/18/20
+            try
+            {
+                LoginDB = await AzuCosmoDBManager.GetUserData();
+                if (LoginDB.Exists(x => x.Email == email.Text))
+                {
+                    if (LoginDB.Exists(x => x.Password == pwd.Text))//Need to ensure it is only for that Data Table
+                    {
+                        try
+                        {
+                            
+                            await Navigation.PushModalAsync(new NavigationPage(new Menu()));
+                        }
+                        catch
+                        {
+                            
+                        }
+
+                    }
+                    else
+                    {
+                        notify.Text = "Wrong Password ";
+                        
+                    }
+                }
+                else
+                {
+                    notify.Text = "Wrong UserName ";
+
+                   
+                }
+            }
+            catch
+            {
+
+            }
+
+            finally
+            {
+                IsBusy = false;
+            }
+
+
+           
+         //   await Navigation.PushModalAsync(new NavigationPage(new Menu())); 10/18/20
+
+        }
+        async Task ExecuteLoginDBVerify()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+            //Copies from Cloud DB to Local List DB 10/18/20
+            try
+            {
+                LoginDB = await AzuCosmoDBManager.GetUserData();
+                if (LoginDB.Exists(x => x.Email == email.Text))
+                {
+                    if (LoginDB.Exists(x => x.Password == pwd.Text))
+                    {
+                        try
+                        {
+                            await Navigation.PushModalAsync(new NavigationPage(new Menu()));
+                        }
+                        catch
+                        {
+
+                        }
+
+                    }
+                    else
+                    {
+                        try
+                        {
+                            await Navigation.PushModalAsync(new NavigationPage(new LoginPage()));
+                        }
+                        catch
+                        {
+
+                        }
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        await Navigation.PushModalAsync(new NavigationPage(new LoginPage()));
+                    }
+                    catch { }
+                }
+            }
+            catch
+            {
+
+            }
+
+            finally
+            {
+                IsBusy = false;
+            }
         }
         async public void DoRegister(object sender, EventArgs e)
         {
             
-            await Navigation.PushModalAsync(new NavigationPage(new NewItemPage()));
+          //  await Navigation.PushModalAsync(new NavigationPage(new NewItemPage()));
             //Items = new ObservableCollection<UserDataModel>();
 
 
@@ -103,7 +199,9 @@ namespace BATTS.Views
             User.Password = pwd.Text;
 
             await AzuCosmoDBManager.InsertUserData(User);
+            notify.Text = "New User Registered";
             await Navigation.PushModalAsync(new NavigationPage(new LoginPage()));
+
         }
 
         async void AddItem_Clicked(object sender, EventArgs e)
